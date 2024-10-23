88b11219a86fd7e3186eb1114c01b73abf4ec815
Chukwuemelie <obioraemelie@gmail.com>
2024-10-23 16:16:18 +0100

    create jsconfig.json file, update vite.config.js file and install shadcn using the command on the shadcn docs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-6xl">Set Up</h1>
       <p className="text-3xl">Just testing</p>
+      <Button>Click me</Button>
     </>
   );
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-// Convert the URL to a file path for the current module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-console.log("Resolved __dirname:", __dirname);
-console.log('Resolved path for "@":', path.resolve(__dirname, "./src"));
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      //external: ["date-fns"],
-    },
-  },
   plugins: [react()],
   resolve: {
     alias: {
