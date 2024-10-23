b75624892280663b05d807f0bae1259e9edf3bfc
Chukwuemelie <obioraemelie@gmail.com>
2024-10-23 15:43:54 +0100

    install dependencies: cors, tailwindcss, shadcn/ui and axios, setup tailwindcss, setup import alias and delete unnecessary react files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-// https://vitejs.dev/config/
+// Convert the URL to a file path for the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log("Resolved __dirname:", __dirname);
+console.log('Resolved path for "@":', path.resolve(__dirname, "./src"));
+
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      //external: ["date-fns"],
+    },
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
